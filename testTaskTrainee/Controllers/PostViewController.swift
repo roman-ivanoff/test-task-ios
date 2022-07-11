353f53148a56a6eb728b747573060e5954f63f84
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-07-11 13:00:25 +0300

    added PostViewController

---
+++ testTaskTrainee/Controllers/PostViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PostViewController.swift
+//  testTaskTrainee
+//
+//  Created by Roman Ivanov on 11.07.2022.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var postTextLabel: UILabel!
+    @IBOutlet weak var daysLabel: UILabel!
+    @IBOutlet weak var likesLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
